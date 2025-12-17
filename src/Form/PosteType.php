ee8fd1622d0db09d6a018f95e06b0989dae5e473
zakina <zakina.rostand@gmail.com>
2025-12-17 20:19:52 +0100

    crud poste

---
+++ src/Form/PosteType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Poste;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PosteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('libelle')
+            ->add('ordre')
+            ->add('tva')
+            ->add('archive')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Poste::class,
+        ]);
+    }
+}
